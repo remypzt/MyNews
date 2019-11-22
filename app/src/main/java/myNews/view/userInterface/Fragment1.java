8f8734e14216791a9f8d04d89bbb90969a870580
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-11-22 11:04:56 +0100

    - I've finish my new attempt to use recyclerview with OC tuto but it still doesnt work

--- app/src/main/java/myNews/view/userInterface/Fragment1.java
+++ app/src/main/java/myNews/view/userInterface/Fragment1.java
@@ -1,22 +1,44 @@
 package myNews.view.userInterface;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import myNews.devexchanges.myNews.R;
+import myNews.service.repositories.model.Articles;
+import myNews.view.adaptater.ArticlesAdapter;
 
 public class Fragment1 extends Fragment {
+
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_recycler_view)
+    RecyclerView recyclerView; // 1 - Declare RecyclerView
+
+    //FOR DATA
+    // 2 - Declare list of users (GithubUser) & Adapter
+    private List<Articles> articles;
+    private ArticlesAdapter adapter;
+
+    public Fragment1() { }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_content, container, false);
+        View view = inflater.inflate(R.layout.fragment_content, container, false);
+        ButterKnife.bind(this, view);
+        this.configureRecyclerView(); // - 4 Call during UI creation
+        return view;
     }
 
     @Override
@@ -25,4 +47,35 @@ public class Fragment1 extends Fragment {
 
 
     }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    // -----------------
+    // CONFIGURATION
+    // -----------------
+
+    // 3 - Configure RecyclerView, Adapter, LayoutManager & glue it together
+    private void configureRecyclerView(){
+        // 3.1 - Reset list
+        this.articles = new ArrayList<>();
+        // 3.2 - Create adapter passing the list of users
+        this.adapter = new ArticlesAdapter(this.articles);
+        // 3.3 - Attach the adapter to the recyclerview to populate items
+        this.recyclerView.setAdapter(this.adapter);
+        // 3.4 - Set layout manager to position the items
+        this.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+
+    // -------------------
+    // UPDATE UI
+    // -------------------
+
+    private void updateUI(List<Articles> users){
+        articles.addAll(users);
+        adapter.notifyDataSetChanged();
+    }
 }
+
