e896869fc94102cbcf61e1c61e27ecfc7c76e538
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-12-05 18:34:26 +0100

    - Start to create viewModel

--- app/src/main/java/myNews/view/Base/BaseFragment.java
+++ app/src/main/java/myNews/view/Base/BaseFragment.java
@@ -2,10 +2,26 @@ package myNews.view.Base;
 
 import androidx.fragment.app.Fragment;
 
+import myNews.data.Injection;
+import myNews.data.repositories.ArticlesRepository;
+
 /**
  * Created by Remy Pouzet on 25/11/2019.
  */
 public class BaseFragment extends Fragment
 {
+    protected ArticlesRepository articlesRepository;
 
+    public BaseFragment()
+    {
+        if (articlesRepository == null) articlesRepository = Injection.createArticlesRepository();
+        this.articlesRepository = articlesRepository;
+    }
+
+    public ArticlesRepository getArticlesRepository()
+    {
+
+        return articlesRepository;
+
+    }
 }
--- app/src/main/java/myNews/viewmodel/ViewModel.java
+++ app/src/main/java/myNews/viewmodel/ViewModel.java
@@ -1,27 +1,11 @@
 package myNews.viewmodel;
 
-import myNews.data.Injection;
-import myNews.data.repositories.ArticlesRepository;
-
 /**
  * Created by Remy Pouzet on 05/12/2019.
  */
 public class ViewModel extends androidx.lifecycle.ViewModel
 {
-    protected ArticlesRepository articlesRepository;
 
-    public ViewModel()
-    {
-        if (articlesRepository == null) articlesRepository = Injection.createArticlesRepository();
-        this.articlesRepository = articlesRepository;
-    }
-
-    public ArticlesRepository getArticlesRepository()
-    {
-
-        return articlesRepository;
-
-    }
 
     // Simplified idea to manage tabs displaying
    /* public List<Articles> displayingAppropriateListOfArticles(){
