f1c53484eec79925b5e32860d65ac8085e1ead35
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-12-26 15:29:46 +0100

    - Implement JellyToolbar and fix somes bugs about it, the application worked but JellyToolbar it doesn't

--- app/src/main/java/myNews/view/activitiesAndFragment/MainActivity.java
+++ app/src/main/java/myNews/view/activitiesAndFragment/MainActivity.java
@@ -1,19 +1,22 @@
 package myNews.view.activitiesAndFragment;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+import com.yalantis.jellytoolbar.listener.JellyListener;
+import com.yalantis.jellytoolbar.widget.JellyToolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,24 +28,51 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @BindView(R.id.view_pager) public ViewPager viewPager;
     @BindView(R.id.drawerLayout) public DrawerLayout drawer;
     @BindView(R.id.tab_layout) public TabLayout tabLayout;
-    @BindView(R.id.toolbar) public Toolbar toolbar;
+    @BindView(R.id.toolbar)
+    public JellyToolbar toolbar;
     @BindView(R.id.nav_view) public  NavigationView navigationView;
 
     private String[] pageTitle = {"Top Stories", "Most Popular", "Technology"};
+
+
+    private JellyToolbar mJellyToolbar;
+    private AppCompatEditText editText;
+    //Jelly toolbar
+    private JellyListener jellyListener = new JellyListener() {
+        @Override
+        public void onCancelIconClicked() {
+            if (TextUtils.isEmpty(editText.getText())) {
+                mJellyToolbar.collapse();
+            } else {
+                editText.getText().clear();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
 
         //create default navigation drawer toggle
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+        /*ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        toggle.syncState();*/
+
+
+        //JellyToolbar
+        mJellyToolbar = findViewById(R.id.toolbar);
+        mJellyToolbar.getToolbar().setNavigationIcon(R.drawable.ic_menu);
+        mJellyToolbar.setJellyListener(jellyListener);
+
+        editText = (AppCompatEditText) LayoutInflater.from(this).inflate(R.layout.edit_text, null);
+        editText.setBackgroundResource(R.color.colorTransparent);
+        mJellyToolbar.setContentView(editText);
+
 
         //setting Tab layout (number of Tabs = number of ViewPager pages)
         for (int i = 0; i < 3; i++) {
