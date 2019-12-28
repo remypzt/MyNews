0adb6b77c0a828b20e58f22de5290c2ad2bae5cd
Votre nom ou pseudo <remy.pouzet@laposte.net>
2019-12-28 17:28:21 +0100

    - revert to 2 commit ago =remove jellytoolbar and my attempt with datapicker because it's dooesn"t appropriate for this project

--- app/src/main/java/myNews/view/activitiesAndFragment/FragmentSearchAndSettings.java
+++ app/src/main/java/myNews/view/activitiesAndFragment/FragmentSearchAndSettings.java
@@ -1,40 +1,7 @@
 package myNews.view.activitiesAndFragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import butterknife.ButterKnife;
-import myNews.myNews.R;
-
 /**
  * Created by Remy Pouzet on 25/12/2019.
  */
-public class FragmentSearchAndSettings extends Fragment {
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        assert getArguments() != null;
-        this.position = getArguments().getInt("position");
-        View view = inflater.inflate(R.layout., container, false); ButterKnife.bind(this, view);
-
-        this.showDatePickerDialog();
-
-        this.showTimePickerDialog();
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-    }
-
-
+public class FragmentSearchAndSettings {
 }
